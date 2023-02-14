45c847b110edd9e412af11873bbf0f4f89dab597
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-02-14 17:08:38 -0300

    Entity Client created

---
+++ src/main/java/com/cmp/clientschallenge/ClientsChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.cmp.clientschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmp/clientschallenge/ClientsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmp.clientschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
